62de457fad00aca07c1e2d2dbee06b3cd09827cd
An.Wang <ViNeverMore@gmail.com>
2023-11-22 14:38:07 +0800

    upgrade: custom_lint etc.

--- lib_ble/example/lib/main.dart
+++ lib_ble/example/lib/main.dart
@@ -18,6 +18,15 @@ void main() async {
 }
 
 void test() {
+  final ff = Text("23");
   final a = Text("1");
   final b = Text("2");
+  final c = Text("3");
+  final d = Text("4");
+  final e = Text("5");
+  final f = Text("6");
+  final g = Text("7");
+  final h = Text("8");
+  final i = Text('9');
+
 }
