63592263325ff8cf0dddc81ac0185a298828ba3c
An.Wang <vinevermore@gmail.com>
2023-11-28 19:33:04 +0800

    update deps

--- lib_ble/example/lib/app.dart
+++ lib_ble/example/lib/app.dart
@@ -15,22 +15,23 @@ class App extends HookConsumerWidget with WidgetsBindingObserver {
           backgroundColor: Colors.green,
         ),
         body: const SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text("1"),
-                Text("2"),
-                Text("3"),
-                Text('3'),
-                Text('4'),
-                Text("5"),
-                Text('6'),
-                Text("7"),
-                Text('8'),
-              ],
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("1"),
+              Text("2"),
+              Text("3"),
+              Text('3'),
+              Text('4'),
+              Text("5"),
+              Text('6'),
+              Text("7"),
+              Text('8'),
+              Text('9'),
+              Text("10"),
+            ],
           ),
-
+        ),
       ),
     );
   }
